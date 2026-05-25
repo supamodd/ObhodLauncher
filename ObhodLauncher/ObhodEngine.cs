62536cebdb10c1c814443bade4f168abd99c248b
supamodd <ghosts.zz@yandex.ru>
2026-05-25 22:51:40 +0300

    upd winws almost fix

--- ObhodLauncher/ObhodEngine.cs
+++ ObhodLauncher/ObhodEngine.cs
@@ -108,11 +108,10 @@ namespace ZapretWPF
 
         private string GetArguments(bool discord, bool youtube, int strategyIndex)
         {
-            // Пути должны быть без пробелов и кавычек в самом пути, но в кавычках при склейке
             string bin = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "bin") + "\\";
             string lists = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "lists") + "\\";
 
-            // Базовые порты (обязательный параметр)
+            // ОБЯЗАТЕЛЬНО ПРОБЕЛ В КОНЦЕ
             string args = $"--wf-tcp=80,443,2053,2083,2087,2096,8443 --wf-udp=443,19294-19344,50000-50100 ";
 
             switch (strategyIndex)
@@ -171,7 +170,7 @@ namespace ZapretWPF
                     }
                     break;
 
-                case 4: // 5. SupaModd Custom (Исправлено)
+                case 4: // 5. SupaModd Custom
                     args += $"--filter-udp=443 --hostlist=\"{lists}list-general.txt\" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-udplen-increment=2 --dpi-desync-any-protocol --new ";
                     args += $"--filter-tcp=80,443 --hostlist=\"{lists}list-general.txt\" --dpi-desync=fake,disorder2 --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq,md5sig --dpi-desync-autohost=sni --new ";
                     if (discord)
@@ -184,10 +183,22 @@ namespace ZapretWPF
                         args += $"--filter-tcp=443 --hostlist=\"{lists}list-google.txt\" --dpi-desync=fake,disorder2 --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq,md5sig --dpi-desync-autohost=sni --new ";
                     }
                     break;
+
+                case 5: // 6. Flowseal ALT 11 (Точная копия из general (ALT11).bat)
+                    args += $"--filter-udp=443 --hostlist=\"{lists}list-general.txt\" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=\"{bin}quic_initial_www_google_com.bin\" --new ";
+                    args += $"--filter-tcp=80,443 --hostlist=\"{lists}list-general.txt\" --dpi-desync=fake,multisplit --dpi-desync-split-seqovl=664 --dpi-desync-split-pos=1 --dpi-desync-fooling=ts --dpi-desync-repeats=8 --dpi-desync-split-seqovl-pattern=\"{bin}tls_clienthello_max_ru.bin\" --dpi-desync-fake-tls=\"{bin}stun.bin\" --dpi-desync-fake-tls=\"{bin}tls_clienthello_max_ru.bin\" --dpi-desync-fake-http=\"{bin}tls_clienthello_max_ru.bin\" --new ";
+                    if (discord)
+                    {
+                        args += $"--filter-udp=19294-19344,50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-fake-discord=\"{bin}quic_initial_dbankcloud_ru.bin\" --dpi-desync-fake-stun=\"{bin}quic_initial_dbankcloud_ru.bin\" --dpi-desync-repeats=6 --new ";
+                        args += $"--filter-tcp=2053,2083,2087,2096,8443 --hostlist-domains=discord.media --dpi-desync=fake,multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=1 --dpi-desync-fooling=ts --dpi-desync-repeats=8 --dpi-desync-split-seqovl-pattern=\"{bin}tls_clienthello_www_google_com.bin\" --dpi-desync-fake-tls=\"{bin}tls_clienthello_www_google_com.bin\" --new ";
+                    }
+                    if (youtube)
+                    {
+                        args += $"--filter-tcp=443 --hostlist=\"{lists}list-google.txt\" --ip-id=zero --dpi-desync=fake,multisplit --dpi-desync-split-seqovl=681 --dpi-desync-split-pos=1 --dpi-desync-fooling=ts --dpi-desync-repeats=8 --dpi-desync-split-seqovl-pattern=\"{bin}tls_clienthello_www_google_com.bin\" --dpi-desync-fake-tls=\"{bin}tls_clienthello_www_google_com.bin\" --new ";
+                    }
+                    break;
             }
 
-            // ВАЖНО: fallback IPSet убран, так как он крашил winws, если файл ipset пустой или не того формата!
-            // Вместо этого просто возвращаем строку как есть (winws.exe автоматически проигнорирует трафик без фильтров).
             return args;
         }
 

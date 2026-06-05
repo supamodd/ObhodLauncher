b56167e257900be7a09d6d202303e9d258657e79
supamodd <ghosts.zz@yandex.ru>
2026-06-05 23:44:53 +0300

    updd 24

--- ObhodLauncher/ObhodEngine.cs
+++ ObhodLauncher/ObhodEngine.cs
@@ -166,7 +166,7 @@ namespace ZapretWPF
                     "31.13.24.0/21\n31.13.64.0/18\n69.63.176.0/20\n69.171.224.0/19\n" +
                     "74.119.76.0/22\n103.4.96.0/22\n129.236.0.0/16\n157.240.0.0/16\n" +
                     "173.252.64.0/18\n179.60.192.0/22\n185.60.216.0/22\n204.15.20.0/22\n" +
-                    // Cloudflare / Fastly / Mindgeek (Pornhub / Models / Gamma)
+                    // Cloudflare / Fastly / Mindgeek 
                     "66.254.114.0/24\n188.114.96.0/20\n104.18.0.0/15\n104.16.0.0/12";
                 File.WriteAllText(mediaIpsetPath, mediaSubnets);
             }
@@ -188,16 +188,14 @@ namespace ZapretWPF
                 args += $"--filter-tcp=80,443,5222,5228 --ipset=\"{listsPrefix}ipset-telegram.txt\" --dpi-desync=split2 --dpi-desync-split-pos=2 --dpi-desync-any-protocol=1 --new ";
                 args += $"--filter-udp=443 --ipset=\"{listsPrefix}ipset-telegram.txt\" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-any-protocol=1 --new ";
 
-                // 2. Инстаграм и Медиа (если пользователь нажал кнопку на 3-й вкладке)
+                // 2. Инстаграм и Медиа
                 if (_enableMediaBypass)
                 {
-                    // Для Инсты и Медиа применяем жесткий fakedsplit с подменой SNI, который пробивает ТСПУ
                     args += $"--filter-tcp=80,443 --ipset=\"{listsPrefix}ipset-media.txt\" --dpi-desync=fake,fakedsplit --dpi-desync-split-pos=1 --dpi-desync-fooling=badseq --dpi-desync-badseq-increment=2 --dpi-desync-repeats=8 --dpi-desync-fake-tls-mod=rnd,dupsid,sni=www.google.com --dpi-desync-fake-http=\"{binPrefix}tls_clienthello_max_ru.bin\" --new ";
                     args += $"--filter-udp=443 --ipset=\"{listsPrefix}ipset-media.txt\" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=\"{binPrefix}quic_initial_www_google_com.bin\" --new ";
                 }
             }
 
-            // Обязательный возврат значения в конце метода!
             return args.Trim();
         
         }

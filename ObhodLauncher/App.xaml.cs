2398fd395ff94cdfcbbdcd6e79e3d7121fcd7d05
supamodd <supamodd@DESKTOP-AE5N24E>
2026-08-29 12:44:16 +0300

    manif upd App.cs

--- ObhodLauncher/App.xaml.cs
+++ ObhodLauncher/App.xaml.cs
@@ -1,14 +1,9 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Forms = System.Windows.Forms;
 
 namespace ObhodLauncher
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
     }
-
 }
